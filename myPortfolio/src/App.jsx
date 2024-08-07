d11c8241566cb6f1840495530b2a2dda7a4f1e87
Anob <anobtakon@gmail.com>
2024-08-07 23:20:42 +0100

    fix the path on the app.js

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,7 +1,7 @@
-import Header from "./Header";
-import About from "./About";
-import Projects from "./Projects";
-import Contact from "./Contact";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
