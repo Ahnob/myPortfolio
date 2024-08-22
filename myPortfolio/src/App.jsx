a5a869d1e2410cd15972809067e230e62817ff86
Anob <anobtakon@gmail.com>
2024-08-22 15:35:19 +0100

    started working on a portfolio

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,21 +1,10 @@
-import Header from "./components/Header";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <About />
-      <Projects />
-      <Contact />
-      <footer className="bg-gray-800 text-white p-4">
-        <div className="container mx-auto text-center">
-          &copy; 2024 My Portfolio. All rights reserved.
-        </div>
-      </footer>
-    </div>
+    <>
+      <div> </div>
+    </>
   );
 }
 
